0de371119f0eaae45fc5acbf56701fdfc8255a10
Scottie Schneider <scott@laclave.co>
2017-09-16 18:52:34 -0500

    ✅ Imported CSS and set up basic containers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './css/style.css';
+import App from './components/App';
+import Header from './components/Header';
+import Order from './components/Order';
+import Inventory from './components/Inventory';
 
 import StorePicker from './components/StorePicker';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
