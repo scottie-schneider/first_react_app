53334aade9e7e271c46d1263f419d3ff2054b512
Scottie Schneider <scott@laclave.co>
2017-09-20 21:49:49 -0500

    Further work

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFound extends React.Component{
+  render(){
+    return (
+      <p>Not Found buddy</p>
+    )
+  }
+}
+
+export default NotFound;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event){
+    event.preventDefault();
+    console.log('You changed the URL');
+    // first grab the text from the box
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
+    // second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
           <h2>Please enter a store</h2>
-          <input type="text" required placeholder="Store Name"/>
+          <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input)=>{ this.storeInput = input}}/>
           <button type="submit">Visit Store -></button>
       </form>
     )
   }
 }
-
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
 export default StorePicker;
